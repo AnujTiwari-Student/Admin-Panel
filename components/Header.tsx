15643fcf415a5e1065c913799c9bbdf582a83ccb
AnujTiwari-Student <anujavengers@gmail.com>
2024-08-06 14:21:59 +0530

    added clerk authentication

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@
 import { CircleHelp, Filter, Home, Menu, X } from 'lucide-react'
 import Image from 'next/image'
 import React, { useState } from 'react'
+import SignIn from './SignIn'
 
 const menu = [
     { name: 'Home', icon: Home, href: '/' },
@@ -19,29 +20,30 @@ function Header() {
     }
 
   return (
-    <div>
+    <div className='flex justify-between lg:justify-end items-center mx-6 my-4 lg:mx-10 lg:my-4'>
 
         <div className='hidden lg:block fixed top-0 left-0 border-r w-64 h-screen bg-white'>
             <div className='p-4 flex justify-center border-b'>
                 {/* <Image src={} alt='' /> */} {/* For the logo */}
-                <h1 className='text-2xl'>Admin Panel</h1>
+                <h1 className='text-2xl font-semibold'>Admin Panel</h1>
             </div>
 
             {menu.map((option)=>(
-                <div key={option.name} className='flex flex-col'>
+                <nav key={option.name} className='flex flex-col'>
                     <div  className='flex space-x-4 mt-4 px-8 bg-blue-600 py-2 rounded-xl text-white mx-4 cursor-pointer'>
                         <option.icon />
                         <p>{option.name}</p>
                     </div>
-                </div>
+                </nav>
             ))}
         </div>
 
-        {/* Moble View */}
 
-        <div className='lg:hidden flex items-center pr-4 z-50 m-4 md:m-6'>
+        {/* Mobile View */}
+
+        <div className='lg:hidden flex items-center'>
             <button onClick={toggleMenu}>
-                {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+                {isMenuOpen ? null : <Menu size={24} />}
             </button>
         </div>
 
@@ -50,7 +52,7 @@ function Header() {
                 <div className='lg:hidden block fixed top-0 left-0 border-r w-64 h-screen bg-white'>
                     <div className='p-4 flex justify-around items-center border-b'>
                         {/* <Image src={} alt='' /> */} {/* For the logo */}
-                        <h1 className='text-2xl'>Admin Panel</h1>
+                        <h1 className='text-2xl font-semibold'>Admin Panel</h1>
 
                         <button onClick={toggleMenu}>
                             {isMenuOpen ? <X size={24} /> : null}
@@ -59,15 +61,17 @@ function Header() {
                     </div>
         
                     {menu.map((option)=>(
-                        <div key={option.name} className='flex flex-col'>
+                        <nav key={option.name} className='flex flex-col'>
                             <div  className='flex space-x-4 mt-4 px-8 bg-blue-600 py-2 rounded-xl text-white mx-4 cursor-pointer'>
                                 <option.icon />
                                 <p>{option.name}</p>
                             </div>
-                        </div>
+                        </nav>
                     ))}
                 </div>
         )}
+
+        <SignIn />
 
     </div>
   )
---
+++ components/SignIn.tsx
@@ -0,0 +1,22 @@
+import { SignedIn, UserButton } from '@clerk/clerk-react'
+import { SignedOut , SignInButton } from '@clerk/nextjs'
+import React from 'react'
+import { Button } from './ui/button'
+
+function SignIn() {
+  return (
+    <div>   
+        <SignedIn>
+            <UserButton/>
+        </SignedIn>
+
+        <SignedOut>
+          <Button asChild variant="default">
+            <SignInButton />
+          </Button>
+        </SignedOut>
+    </div>
+  )
+}
+
+export default SignIn
